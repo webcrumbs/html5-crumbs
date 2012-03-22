45e2a82b36afc67da63bf3b5e27c408a59b774d2
Federico Spini <federico.spini@gmail.com>
2012-03-22 15:57:34 +0100

    updated steop05

--- step05/js/script.js
+++ step05/js/script.js
@@ -9,6 +9,20 @@
   var button_green = $('#button-green');
   var button_blue = $('#button-blue');
 
+  var button_white = $('#button-white');
+  var button_gray = $('#button-gray');
+  var containers = $('.container');
+
+  button_white.on('click', function (e) {
+    containers.removeClass('bg-gray');
+    containers.addClass('bg-white');
+  });
+
+  button_gray.on('click', function (e) {
+    containers.removeClass('bg-white');
+    containers.addClass('bg-gray');
+  });
+
   button_red.on('click', function (e) {
     ctx.clearRect(0, 0, width, height);
     ctx.fillStyle = 'rgb(255,0,0)';
